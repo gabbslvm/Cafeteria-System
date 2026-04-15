e3886d409852a48928adf257c20f54fee4efe497
soldat-panther <baldonidos@gmail.com>
2026-04-15 17:38:15 +0800

    Update PaymentService.java

--- src/main/java/service/PaymentService.java
+++ src/main/java/service/PaymentService.java
@@ -1,5 +1,20 @@
 package service;
 
 public class PaymentService {
-    
+
+    private static final double STUDENT_DISCOUNT_RATE = 0.20; 
+    public double applyStudentDiscount(double total) {
+        double discountAmount = total * STUDENT_DISCOUNT_RATE;
+        return total - discountAmount;
+    }
+    public boolean isPaymentSufficient(double paid, double total) {
+        return paid >= total;
+    }
+
+    public double computeChange(double paid, double total) {
+        if (isPaymentSufficient(paid, total)) {
+            return paid - total;
+        }
+        return 0.0; 
+    }
 }
